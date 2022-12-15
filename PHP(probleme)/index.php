bc656628bdcb074519de32e7897a031ca3185c42
Pholien Victor <180229@CCM-Asty-Moulin.local>
2022-12-15 13:12:03 +0100

    Ajout du Etape4

---
+++ PHP(probleme)/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Victor Pholien</title>
+</head>
+<body>
+    <?php 
+        $m = 700; 
+        while($m == 0) :
+            $m % 500 ?>
+       
+</body>
+</html>
